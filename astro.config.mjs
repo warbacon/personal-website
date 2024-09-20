3392ab4791a03c393e0b9cfd3539189da2f88206
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-09-20 18:34:04 +0200

    Fix vercel 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
+    adapter: vercel(),
     integrations: [tailwind()],
-
     env: {
         schema: {
             GITHUB_TOKEN: envField.string({ context: "server", access: "secret" }),
         },
     },
-
-    adapter: vercel(),
 });
