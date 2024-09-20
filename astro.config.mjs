3fd120a7de16eabaf59eca8e557b54cfec103611
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-09-20 18:21:39 +0200

    Upgrade to astro 5.0 and use server islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    output: "server",
     integrations: [tailwind()],
+    env: {
+        schema: {
+            GITHUB_TOKEN: envField.string({ context: "server", access: "secret" }),
+        },
+    },
 });
