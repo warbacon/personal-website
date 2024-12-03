c3a10fd27a2c9e94346705ee3d48a276146ee56d
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-12-03 22:03:15 +0100

    Fix vercel ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    adapter: vercel(),
     env: {
         schema: {
             GITHUB_TOKEN: envField.string({ context: "server", access: "secret" }),
